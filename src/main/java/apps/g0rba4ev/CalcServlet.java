338a448231c60e2ebd9f7df409247f27a4f3f8c4
Gorbachev Ilya <g00d_men@mail.ru>
2020-01-07 03:30:56 +0300

    Change servlet mapping from description in web.xml to @WebServlet annotation
    Encountered a problem in the process: @WebServlet annotation ingoring when attribute "metadata-complete" in web.xml is "true"
    
    There is from stackoverflow (from Java Servlet 3.0 / 3.1 Specification):
    "...If metadata-complete is set to "true", the deployment tool only examines the web.xml file and must ignore annotations such as @WebServlet..."

--- src/main/java/apps/g0rba4ev/CalcServlet.java
+++ src/main/java/apps/g0rba4ev/CalcServlet.java
@@ -1,12 +1,13 @@
 package apps.g0rba4ev;
 
 import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.io.PrintWriter;
 
+@WebServlet("/doCalc")
 public class CalcServlet extends HttpServlet {
 
     enum State {
